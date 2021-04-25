871e901b810fc80bcf1a3e289f2456e013c0ee11
Thomas De Maesschalck <thomas.dm@dvhardware.net>
2021-04-25 14:45:12 +0200

    Project start - Initial commit

---
+++ game/src/test/java/be/hogent/programmeren5/connectfour/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package be.hogent.programmeren5.connectfour.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ player/src/test/java/be/hogent/programmeren5/connectfour/player/PlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.hogent.programmeren5.connectfour.player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
